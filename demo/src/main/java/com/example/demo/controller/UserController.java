7b8b063fbb89f6b7b17e8fcd0c29099801d9b5e0
SonTT119 <sonden02092002@gmail.com>
2023-11-26 23:42:43 +0700

    user_stock

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/users/name/{userName}")
+    public List<User> getUserByName(@PathVariable String userName) {
+        return userService.getUserByName(userName);
+    }
+
+    @PostMapping("/users")
+    public void saveOrUpdate(@RequestBody User user) {
+        userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void delete(@PathVariable long id) {
+        userService.delete(id);
+    }
+}
--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,49 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public User(String userName, String password, String email) {
+        super();
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email + "]";
+    }
+
 }
